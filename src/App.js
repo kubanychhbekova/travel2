e4286c63ba124e8d0f18b3485e2e5e5ee5d03d3d
Nursultan <amanturovnursultan458@gmail.com>
2023-07-19 13:50:59 +0600

    route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import './App.scss';
+import "./App.scss";
 import Header from "../../travel2/src/components/header";
 import Footer from "../../travel2/src/components/footer/footer";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Pages from "../../travel2/src/components/pages/pages";
 import Hotels from "../../travel2/src/components/hotels/hotels";
 import Detail from "../../travel2/src/components/popular/popularDetails/detail";
 import HotelDetail from "../../travel2/src/components/hotels/hotelDetail/hotelDetail";
-
+import AboutRoutes from "./components/aboutRoutes/aboutRoutes";
 
 function App() {
   return (
-      <div className="App">
-        <Header/>
-        <Routes>
-          <Route path={"/"} element={<Pages/>}/>
-          <Route path={"/hotels"} element={<Hotels/>}/>
-          <Route path={"/popular/:id"} element={<Detail/>}/>
-          <Route path={"/hotels/:id"} element={<HotelDetail/>}/>
-        </Routes>
-        <Footer/>
-      </div>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Pages />} />
+        <Route path={"/hotels"} element={<Hotels />} />
+        <Route path={"/popular/:id"} element={<Detail />} />
+        <Route path={"/hotels/:id"} element={<HotelDetail />} />
+        <Route path={"/aboutRoutes"} element={<AboutRoutes />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/aboutRoutes/aboutRoutes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Zagalovok from "./zagalovok/Zagalovok";
+import Content from "./content/Content";
+const AboutRoutes = () => {
+  return (
+    <div>
+        <Zagalovok/>
+        <Content/>
+    </div>
+  );
+};
+export default AboutRoutes;
